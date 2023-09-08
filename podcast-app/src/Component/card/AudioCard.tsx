2386950fd6ec283acff64ada1de431d7129b22bb
Love Trivedi <lovetrivedi@Loves-MacBook-Air.local>
2023-09-08 11:20:01 +0530

    improve constants

--- podcast-app/src/Component/card/AudioCard.tsx
+++ podcast-app/src/Component/card/AudioCard.tsx
@@ -1,7 +1,7 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import AudioData from '../../constant/podcastData.json';
+import {Const_Pod_Cast_Data} from '../../constant/PodCastData.ts';
 import { Typography } from '@mui/material';
 import { AspectRatio } from '@mui/joy';
 import { useDispatch } from 'react-redux';
@@ -9,13 +9,13 @@ import Grid from '@mui/material/Unstable_Grid2/Grid2';
 import { getItemDetails } from '../../services/redux/slice/Slice';
 import { Link } from 'react-router-dom';
 const AudioCard = () => {
-    console.log("Audio data is  : ", AudioData);
+    console.log("Audio data is  : ", Const_Pod_Cast_Data);
     const dispatch = useDispatch();
     return (
         <Grid container sx={{ display: 'flex' }}>
             <Grid xs={9} sx={{ display: "flex", flexWrap: "wrap", justifyContent: "flex-start", alignItems: "center", bgcolor: '#121212', pl: 6, pt: 4, pb: 4, overflowY: 'scroll', height: "620px" }}>
                 {
-                    AudioData.map((item, index: number) => {
+                    Const_Pod_Cast_Data.map((item, index: number) => {
                         return (
                             <Card sx={{ width: 200, ml: 3, mt: 3 }} key={index} onClick={() => { dispatch(getItemDetails(item)) }}>
                                 <Link to="/podcast" style={{ textDecoration: 'none' }}>
